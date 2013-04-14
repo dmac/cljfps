b52a43bfcefc0c510820ef967ca50241f9172cd7
Daniel MacDougall <dmacdougall@gmail.com>
2013-04-14 16:10:43 -0700

    Use resource helper for loading textures

--- src/fps/textures.clj
+++ src/fps/textures.clj
@@ -1,5 +1,6 @@
 (ns fps.textures
   (:require [clojure.string :as string])
+  (:use [clojure.java.io :only [resource]])
   (:import java.io.File
            [org.newdawn.slick.opengl TextureLoader]
            [org.newdawn.slick.util ResourceLoader]))
@@ -10,7 +11,7 @@
   (TextureLoader/getTexture "PNG" (ResourceLoader/getResourceAsStream filename)))
 
 (defn load-textures []
-  (let [texture-files (->> (File. "resources/textures") .listFiles (map #(.getName %)))]
+  (let [texture-files (->> (File. (.getFile (resource "textures"))) .listFiles (map #(.getName %)))]
     (doseq [texture-file texture-files]
       (let [texture-key (-> texture-file (string/split #"\.") first keyword)
             texture (load-texture (str "textures/" texture-file))]
