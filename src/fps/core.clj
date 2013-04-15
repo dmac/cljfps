fd25f3413d3c58b8b8c11c7fd0196e2f8253d1f5
Daniel MacDougall <dmacdougall@gmail.com>
2013-04-15 12:43:16 -0700

    Split out graphics and util functions from core

--- src/fps/core.clj
+++ src/fps/core.clj
@@ -1,55 +1,21 @@
 (ns fps.core
-  (:use [fps.component-entity]
-        [fps.textures :only [load-textures get-texture]])
-  (:import [java.nio ByteBuffer ByteOrder]
-           [org.lwjgl Sys]
-           [org.lwjgl.opengl Display DisplayMode GL11]
-           [org.lwjgl.util.glu GLU]
-           [org.lwjgl.input Keyboard Mouse]
-           [org.newdawn.slick.opengl TextureLoader]
-           [org.newdawn.slick.util ResourceLoader])
+  (:require [fps.graphics :as graphics])
+  (:use fps.component-entity
+        [fps.levels :only [load-level]]
+        [fps.util :only [find-first float-buffer]])
+  (:import [org.lwjgl Sys]
+           [org.lwjgl.opengl Display DisplayMode]
+           [org.lwjgl.input Keyboard Mouse])
   (:gen-class))
-
-; TODO
-; - collision detection with AABBs
 
 (def movement-speed 10) ; meters per second
 (def mouse-sensitivity 0.1)
-
-(defn- find-first [pred coll]
-  (first (filter pred coll)))
-
-(defn- float-buffer [& args]
-  (-> (doto (ByteBuffer/allocateDirect 16) (.order (ByteOrder/nativeOrder)))
-      .clear .asFloatBuffer
-      (.put (float-array args))
-      .flip))
-
-(defn- select-indices [coll indices]
-  (let [selected-map (select-keys coll indices)]
-    (into [] (map #(get selected-map %) indices))))
 
 (defn- get-time []
   (-> (Sys/getTime) (/ (Sys/getTimerResolution)) (* 1000) double))
 
 (defn- distance-delta [dt]
   (-> dt (/ 1000) (* movement-speed)))
-
-(defn- set-color [[r g b]]
-  (GL11/glColor3f r g b))
-
-(defn- clear-screen []
-  (GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT GL11/GL_DEPTH_BUFFER_BIT))
-  (GL11/glLoadIdentity))
-
-(defn- bounding-points [{{:keys [x y z]} :position
-                         {:keys [width height depth]} :volume :as entity}]
-  {:pre [(and (:position entity) (:volume entity))]}
-  (let [hw (/ width 2) hh (/ height 2) hd (/ depth 2)]
-    [[(- x hw) (- y hh) (+ z hd)] [(- x hw) (+ y hh) (+ z hd)]
-     [(+ x hw) (+ y hh) (+ z hd)] [(+ x hw) (- y hh) (+ z hd)]
-     [(- x hw) (- y hh) (- z hd)] [(- x hw) (+ y hh) (- z hd)]
-     [(+ x hw) (+ y hh) (- z hd)] [(+ x hw) (- y hh) (- z hd)]]))
 
 (defn- collides? [{{x1 :x y1 :y z1 :z} :position {w1 :width h1 :height d1 :depth} :volume :as entity1}
                   {{x2 :x y2 :y z2 :z} :position {w2 :width h2 :height d2 :depth} :volume :as entity2}]
@@ -59,33 +25,9 @@
        (<= (Math/abs (- y1 y2)) (+ (/ h1 2) (/ h2 2)))
        (<= (Math/abs (- z1 z2)) (+ (/ d1 2) (/ d2 2)))))
 
-(defn- draw-box [{{:keys [texture]} :texture :as box}]
-  (set-color [1.0 1.0 1.0])
-  (when texture
-    (GL11/glBindTexture GL11/GL_TEXTURE_2D (.getTextureID (get-texture texture))))
-  (GL11/glPushMatrix)
-  (GL11/glBegin GL11/GL_QUADS)
-  (let [points (bounding-points box)
-        texture-points [[0 0] [0 1] [1 1] [1 0]]]
-    (doseq [point-indices [[0 1 2 3] [4 5 1 0] [7 6 5 4] [3 2 6 7] [3 7 4 0] [1 5 6 2]]]
-      (doseq [[[tx ty] [x y z]] (partition 2 (interleave texture-points
-                                                         (select-indices points point-indices)))]
-        (GL11/glTexCoord2f tx ty)
-        (GL11/glVertex3f x y z))))
-  (GL11/glEnd)
-  (GL11/glPopMatrix)
-  (GL11/glBindTexture GL11/GL_TEXTURE_2D 0))
-
-(defn- draw [{:keys [entities]}]
+(defn- render-all [{:keys [entities]}]
   (doseq [entity (filter :render (vals entities))]
     ((get-in entity [:render :fn]) entity)))
-
-(defn- look-through [{{:keys [x y z]} :position {:keys [pitch yaw]} :orient :as entity}]
-  {:pre [(and (:position entity) (:orient entity))]}
-  (GL11/glRotatef pitch 1 0 0)
-  (GL11/glRotatef yaw 0 1 0)
-  ; The 0.8 adjustment increases the height of the camera "eyes" to near the top of the player entity.
-  (GL11/glTranslatef (- x) (- (+ y 0.8)) (- z)))
 
 (defn- move [entity direction distance collidables]
   {:pre [(and (:position entity) (:orient entity))
@@ -185,79 +127,25 @@
           :when (and (:flight entity) (:velocity entity))]
       [:entities entity-id])))
 
-(defn- tick [game dt]
+(defn- update-all [game dt]
   (update-flight game dt))
 
-(defn- init-gl []
-  (GL11/glMatrixMode GL11/GL_PROJECTION)
-  (GL11/glLoadIdentity)
-  (GLU/gluPerspective 70.0 (/ 800 600) 0.1 1000)
-  (GL11/glMatrixMode GL11/GL_MODELVIEW)
-  (GL11/glClearColor 0 0 0 0)
-  (GL11/glClearDepth 1)
-  (GL11/glEnable GL11/GL_DEPTH_TEST)
-  (GL11/glDepthFunc GL11/GL_LEQUAL)
-  (GL11/glHint GL11/GL_PERSPECTIVE_CORRECTION_HINT GL11/GL_NICEST)
-  (GL11/glEnable GL11/GL_TEXTURE_2D)
-  ;; Lighting
-  ;(GL11/glEnable GL11/GL_LIGHTING)
-  ;(GL11/glEnable GL11/GL_COLOR_MATERIAL)
-  ;(GL11/glColorMaterial GL11/GL_FRONT_AND_BACK GL11/GL_AMBIENT_AND_DIFFUSE)
-  ;(GL11/glLight GL11/GL_LIGHT0 GL11/GL_POSITION (float-buffer 5 5 0 1))
-  ;(GL11/glEnable GL11/GL_LIGHT0)
-  (load-textures))
-
-(defn- init-window [w h]
-  (Display/setDisplayMode (DisplayMode. w h))
-  (Display/setTitle "FPS")
-  (Display/setVSyncEnabled true)
-  (Display/create)
-  (Mouse/setGrabbed true))
-
 (defn run []
-  (init-window 1024 768)
-  (init-gl)
+  (graphics/init-window 1024 768)
+  (graphics/init-gl)
   (loop [last-time (get-time)
          game {:entities (into {} (map (fn [entity] [(:id entity) entity])
-                                       [(entity :player
-                                          (position :x 5 :y 2 :z 10)
-                                          (volume :width 1 :height 1.99 :depth 1)
-                                          (orient :pitch 0 :yaw 0)
-                                          (velocity :vy 0)
-                                          (flight :airborn false))
-                                        (entity :floor
-                                          (position :x 0 :y 0 :z 0)
-                                          (volume :width 100 :height 0 :depth 100))
-                                        (entity :box
-                                          (position :x 5 :y 5 :z -5)
-                                          (volume :width 10 :height 10 :depth 10)
-                                          (texture :texture :stone)
-                                          (render :fn draw-box))
-                                        (entity :box2
-                                          (position :x -1 :y 1 :z -1)
-                                          (volume :width 2 :height 2 :depth 2)
-                                          (texture :texture :crate)
-                                          (render :fn draw-box))
-                                        (entity :box3
-                                          (position :x -1 :y 3 :z -3)
-                                          (volume :width 2 :height 2 :depth 2)
-                                          (texture :texture :crate)
-                                          (render :fn draw-box))
-                                        (entity :box4
-                                          (position :x -1 :y 5 :z -5)
-                                          (volume :width 2 :height 2 :depth 2)
-                                          (texture :texture :crate)
-                                          (render :fn draw-box))
-                                        (entity :box5
-                                          (position :x -1 :y 7 :z -7)
-                                          (volume :width 2 :height 2 :depth 2)
-                                          (texture :texture :crate)
-                                          (render :fn draw-box))
-                                        (entity :box6
-                                          (position :x -1 :y 9 :z -9)
-                                          (volume :width 2 :height 2 :depth 2)
-                                          (texture :texture :crate)
-                                          (render :fn draw-box))]))}]
+                                       (concat
+                                         [(entity :player
+                                            (position :x 5 :y 2 :z 10)
+                                            (volume :width 0.5 :height 1.9 :depth 0.5)
+                                            (orient :pitch 0 :yaw 0)
+                                            (velocity :vy 0)
+                                            (flight :airborn false))
+                                          (entity :floor
+                                            (position :x 0 :y 0 :z 0)
+                                            (volume :width 100 :height 0 :depth 100))]
+                                         (load-level "room.dat"))))}]
     (if (Display/isCloseRequested)
       (System/exit 0)
       ;(Display/destroy)
@@ -268,10 +156,10 @@
                            handle-mouse-input
                            (handle-keyboard-input (get-key-buffer))
                            (handle-constant-keyboard-input dt)
-                           (tick dt))]
-          (clear-screen)
-          (look-through (get-in game [:entities :player]))
-          (draw new-game)
+                           (update-all dt))]
+          (graphics/clear-screen)
+          (graphics/look-through (get-in game [:entities :player]))
+          (render-all new-game)
           (Display/update)
           (Display/sync 60)
           (recur new-time new-game))))))
