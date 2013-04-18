ab252bfa392e39f7ff9572e81ac98502de2cb350
Daniel MacDougall <dmacdougall@gmail.com>
2013-04-17 18:23:25 -0700

    Render textures with VBOs

--- src/fps/graphics.clj
+++ src/fps/graphics.clj
@@ -20,14 +20,14 @@
   (GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT GL11/GL_DEPTH_BUFFER_BIT))
   (GL11/glLoadIdentity))
 
-(defn- set-color [[r g b]]
-  (GL11/glColor3f r g b))
-
+; TODO: To make this a little more sane, maybe use two VBOs instead of one interleaved VBO.
 (defn regenerate-box-vertex-data! [box]
   (let [points (systems/bounding-points box)
         face-indices [[0 1 2 3] [4 5 1 0] [7 6 5 4] [3 2 6 7] [3 7 4 0] [1 5 6 2]]
+        texture-points [[0 0] [0 1] [1 1] [1 0]]
         vertex-data (->> face-indices
-                         (map (fn [corner-indices] (select-indices points corner-indices)))
+                         (map (partial select-indices points))
+                         (map #(interleave % texture-points))
                          (apply concat)
                          (apply concat)
                          (apply float-buffer))]
@@ -36,14 +36,23 @@
     box))
 
 (defn draw-box [box]
-  ; Consider having each face be its own VBO and only draw visible ones
-  (let [vertex-size 3
+  ; [x y z tx ty]
+  (let [sizeof-float 4
+        vertex-size 3
+        texture-size 2
+        stride (* sizeof-float (+ vertex-size texture-size))
+        vertex-offset 0
+        texture-offset (* vertex-size sizeof-float)
         num-vertices 24]
-    (set-color [1.0 1.0 1.0])
+    ; Consider having each face be its own VBO and only draw visible ones
     (GL11/glPushMatrix)
+    ; TODO: This line drops FPS from 60 to 30. Profile with warn-on-reflection.
+    (GL11/glBindTexture GL11/GL_TEXTURE_2D (.getTextureID (get-texture (get-in box [:material :material]))))
     (GL15/glBindBuffer GL15/GL_ARRAY_BUFFER (get-in box [:render :vertex-buffer-id]))
-    (GL11/glVertexPointer vertex-size GL11/GL_FLOAT 12 0)
+    (GL11/glVertexPointer vertex-size GL11/GL_FLOAT stride vertex-offset)
+    (GL11/glTexCoordPointer texture-size GL11/GL_FLOAT stride texture-offset)
     (GL11/glDrawArrays GL11/GL_QUADS 0 num-vertices)
+    ;(GL11/glBindTexture GL11/GL_TEXTURE_2D 0)
     (GL11/glPopMatrix)))
 
 (defn look-through [{{:keys [x y z]} :position {:keys [pitch yaw]} :orient :as entity}]
@@ -72,6 +81,7 @@
   (GL11/glHint GL11/GL_PERSPECTIVE_CORRECTION_HINT GL11/GL_NICEST)
   (GL11/glEnable GL11/GL_TEXTURE_2D)
   (GL11/glEnableClientState GL11/GL_VERTEX_ARRAY)
+  (GL11/glEnableClientState GL11/GL_TEXTURE_COORD_ARRAY)
   ;; Lighting
   ;(GL11/glEnable GL11/GL_LIGHTING)
   ;(GL11/glEnable GL11/GL_COLOR_MATERIAL)
