a632bed2e9a4561589794661f951c27fd2c709a3
Daniel MacDougall <dmacdougall@gmail.com>
2018-11-02 01:32:20 -0700

    Update lwjgl

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
-                 [org.lwjgl.lwjgl/lwjgl "2.8.5"]
-                 [org.lwjgl.lwjgl/lwjgl_util "2.8.5"]
-                 [org.lwjgl.lwjgl/lwjgl-platform "2.8.5" :classifier "natives-osx" :native-prefix ""]
+                 [org.lwjgl/lwjgl "3.2.0"]
+                 [org.lwjgl.lwjgl/lwjgl_util "2.9.3"]
+                 [org.lwjgl.lwjgl/lwjgl-platform "2.9.3" :classifier "natives-osx" :native-prefix ""]
                  [slick-util "1.0.0"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]
                    :plugins [[lein-midje "3.0.1"]]}}
